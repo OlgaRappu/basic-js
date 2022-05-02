739f0532354deab75f1ef30c7119c0b7e92a52f2
Ольга Дробышевская <95051051+OlgaRappu@users.noreply.github.com>
2022-05-02 21:11:54 +0000

    fix:extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) 
+{
+let res = '';
+  str = String(str);
+  let separator;
+  let addition;
+  if (options !=undefined)
+  if (options.repeatTimes != undefined) n = options.repeatTimes;
+  else n = 0;
+  if (options.separator != undefined) separator = options.separator;
+  else separator = '+';
+  if (options.addition != undefined) addition = String(options.addition);
+  else if (options.addition === null) addition = 'null'; 
+  else addition = '';
+  if (options.additionRepeatTimes != undefined) m = 
+   options.additionRepeatTimes;
+    else  m=0;
+  if (options.additionSeparator != undefined)  additionSeparator = 
+  options.additionSeparator;
+   else additionSeparator = '|';
+   for (i=0;i<n;i++ )
+   {res = res+str+addition; ;
+    for(j=0;j<m-1;j++)
+    {     res=res+additionSeparator+addition;     }
+    if (i<(n-1))  res=res+separator;
+  }
+  if (n==0 && m ==0)
+  res = str+addition;
+  return res;
+  
 }
-
 module.exports = {
   repeater
 };
